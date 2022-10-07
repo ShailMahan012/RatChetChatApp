03ac5dffaa719c3c0a17eac08d7f1ffa4d7f160e
Shail Murtaza <shailmahan012@gmail.com>
2022-10-07 20:19:57 +0500

    Incomplete Contacs

--- app/contacts.php
+++ app/contacts.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/contacts.css">
     <title>Contacts</title>
 </head>
 <body>
     <div class="container">
         <div class="inner-container">
-
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search User" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">Search</button>
+                </div>
+            </div>
         </div>
     </div>
 </body>
